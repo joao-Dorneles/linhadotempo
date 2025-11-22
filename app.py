0f06f769c387f7157076acfe65ede5921805ca5c
joao-Dorneles <joaopedrodvv@gmail.com>
2025-11-22 18:08:04 -0300

    adicao do arquivo xml

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml', mimetype='application/xml')
+
 if __name__ == "__main__":
     app.run(debug=True)
