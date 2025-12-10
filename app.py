38edb7e3f14d05d8ad5f80716f88d23e849f8326
joao-Dorneles <joaopedrodvv@gmail.com>
2025-12-09 21:55:18 -0300

    tentativa que precisa ser a unica de fazer a pesquisa funcionar por proxy

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify, request, Response
 import os
+import requests
 
 app = Flask(__name__)
+
+CARDAPIO_BASE_URL = "https://usercenterlt.onrender.com"
 
 @app.route("/")
 def index():
@@ -10,6 +13,22 @@ def index():
 @app.route('/sitemap.xml')
 def sitemap():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml', mimetype='application/xml')
+
+@app.route("/proxy-search")
+def proxy_search():
+    termo = request.args.get('q', '') 
+    target_url = f"{CARDAPIO_BASE_URL}/api/search?q={termo}"
+    
+    try:
+        response = requests.get(target_url)
+        proxy_response = Response(response.content)
+        proxy_response.headers['Content-Type'] = response.headers.get('Content-Type', 'application/json')
+        
+        return proxy_response
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao conectar com o Domínio 2: {e}")
+        return jsonify({"error": "Falha na comunicação com o servidor de Cardápio."}), 503
 
 if __name__ == "__main__":
     app.run(debug=True)
